7635abfcc1c0f5b37e879e105e242fe7d0cf057b
Felix Fietkau <nbd@nbd.name>
2022-12-15 13:57:04 +0100

    OPENWRT:PROTOCOL:hostapd: add support for automatically setting RADIUS own-ip dynamically
    Some servers use the NAS-IP-Address attribute as a destination address
    
    TAG: Security
    
    clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/openwrt_patches/760-dynamic_own_ip.patch
    
    Change-Id: I6eab9996c686bf270308892688a57de45cdaa79b
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

--- src/ap/ap_config.h
+++ src/ap/ap_config.h
@@ -312,6 +312,7 @@ struct hostapd_bss_config {
 	unsigned int eap_sim_db_timeout;
 	int eap_server_erp; /* Whether ERP is enabled on internal EAP server */
 	struct hostapd_ip_addr own_ip_addr;
+	int dynamic_own_ip_addr;
 	char *nas_identifier;
 	struct hostapd_radius_servers *radius;
 	int radius_require_message_authenticator;
