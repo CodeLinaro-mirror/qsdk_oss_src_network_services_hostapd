7df44f05e37ce4a5eb206b0cdaffcbc69ba439eb
Sri Vidya Gunturi <sgunturi@qti.qualcomm.com>
2025-08-14 14:37:53 +0530

    UPSTREAM:PROTOCOL: hostapd: Terminate the EPCS SDWF session only if it is enabled
    Terminate the EPCS SDWF Session, only if the current EPCS state of the
    MLD STA is EPCS_STATE_ENABLED. Reset the EPCS state of MLD STA to
    EPCS_STATE_DISABLED after the SDWF session is successfully terminated.
    
    TAG: EPCS
    
    clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/patches/r99-UPSTREAM-PROTOCOL-hostapd-Terminate-the-EPCS-SDWF-se.patch
    Gerrit: https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/6213783
    
    Fixes: Ib510869b17353f1176e47927ab5414549170c2cd (UPSTREAM:PROTOCOL: hostapd: Configure nftables for EPCS)
    
    Change-Id: Ic882f7d6398f3e06ff010bc3891dc1f2031738ad
    Signed-off-by: Sri Vidya Gunturi <sgunturi@qti.qualcomm.com>
    Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

--- src/ap/sta_info.c
+++ src/ap/sta_info.c
@@ -673,9 +673,12 @@ void ap_free_sta(struct hostapd_data *hapd, struct sta_info *sta)
 	os_free(sta->ifname_wds);
 
 #ifdef CONFIG_IEEE80211BE
-
-	wpa_printf(MSG_INFO, "EPCS Tear Down from AP free STA");
-	hostapd_configure_epcs(hapd, sta, QM_REMOVE_REQ);
+	if (sta->mld_info.mld_sta &&
+	    sta->mld_info.epcs.state == EPCS_STATE_ENABLED) {
+		wpa_printf(MSG_INFO, "EPCS Tear Down from AP free STA");
+		if (!hostapd_configure_epcs(hapd, sta, QM_REMOVE_REQ))
+			sta->mld_info.epcs.state = EPCS_STATE_DISABLED;
+	}
 	eloop_cancel_timeout(hostapd_epcs_timeout_handler, hapd, sta);
 	ap_sta_free_sta_profile(&sta->mld_info);
 	ml_deinit_link_reconf_req(&sta->reconf_req);
