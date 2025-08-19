a521e88a57eaf7bb460fd4ecc7f9c2cb2313d421
Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>
2025-08-19 19:35:24 +0530

    UPSTREAM:PROTOCOL:hostapd: Fix use-after-free and update mac address in Link Reconfiguration Response.
    Currently when handling station link removal in Link Reconfiguration
    Request, ap_free_sta() is called before logging
    WPA_EVENT_LINK_STA_REMOVED. This can lead to use-after-free or invalid
    memory access when printing the STA address in the log message.
    
    Move ap_free_sta() after the log message to ensure the STA address
    remains valid during logging.
    
    Also fix an issue in Link Reconfiguration Response where the STA address
    in the per-STA profile was encoded as an invalid mac
    address (00:00:00:00:00:00). Copy the hostapd link address into the per
    STA profile to ensure the correct address is included.
    
    TAG: GENERAL
    
    clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/patches/s002-UPSTREAM-PROTOCOL-hostapd-Fix-use-after-free-and-upd.patch
    Gerrit: https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/6223592
    
    Patch-dependency: patches/r93-b-004-UPSTREAM-PROTOCOL-HOSTAPD-Fix-ML-reconfig-response-f.patch
    
    Patch-work: None
    
    Fixes: Ia7bb63df9c38(6131166: PROTOCOL: MLD: Add support to handle ML Setup Link Reconfiguration)
    
    Change-Id: I924e73c088a0137d2fef364603bca1425bfc6322
    Signed-off-by: Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>
    Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

--- src/ap/ieee802_11_eht.c
+++ src/ap/ieee802_11_eht.c
@@ -1789,10 +1789,10 @@ void hostapd_link_reconf_resp_tx_status(struct hostapd_data *hapd,
 		}
 
 		/* Free as a link STA */
-		ap_free_sta(lhapd, lsta);
 		wpa_msg(hapd->msg_ctx, MSG_INFO,
 			WPA_EVENT_LINK_STA_REMOVED "sta=" MACSTR " link_id=%u",
 			MAC2STR(lsta->addr), link_id);
+		ap_free_sta(lhapd, lsta);
 
 		for_each_mld_link(other_hapd, lhapd) {
 			struct mld_link_info *link;
@@ -2017,6 +2017,8 @@ hostapd_send_link_reconf_resp(struct hostapd_data *hapd,
 				continue;
 
 			link->valid = true;
+
+			os_memcpy(link->local_addr, lhapd->own_addr, ETH_ALEN);
 			ieee80211_ml_build_assoc_resp(lhapd, NULL, assoc_sta, link);
 		}
 		/* TODO: Basic MLE is not supposed to include BPCC in Link
