b2c69deb86069c84494398231d8b3e7f97ef3d1b
Sai Pratyusha Magam <smagam@qti.qualcomm.com>
2025-11-16 21:47:01 +0530

    UPSTREAM:PROTOCOL:hostapd: Use appropriate VLAN group instead of default VLAN group
    The current implementation in the GTK Rekey path refers to
    the default VLAN group instead of the VLAN to which the
    client is bound to. For instance, the GKeyDoneStations
    count is incorrectly incremented/decremented for partner
    links in the default VLAN group always, even when the
    station has associated on the non-zero VLAN. This can
    result in inconsistencies during GTK Rekey for stations
    connected on non-zero VLAN where the new group keys are
    installed to the driver even before they could be
    communicated to the stations on these VLANs through a
    group handshake message 1.
    
    At all of those places, let the correct VLAN group be
    referenced based on the vlan_id to which the station is
    connected to, instead of using the default VLAN.
    
    Patch-dependency: Ief2639b4c0a3005e1035c87d23c591931b98691f ("UPSTREAM:PROTOCOL:hostapd: VLAN group state machine data creation for partner AP MLD links")
    
    Change-Id: I516fe072ae210472cb481c7eb1ed6cb041eaea54
    Signed-off-by: Sai Pratyusha Magam <smagam@qti.qualcomm.com>

--- src/ap/wpa_auth.c
+++ src/ap/wpa_auth.c
@@ -98,8 +98,14 @@ static void wpa_gkeydone_sta(struct wpa_state_machine *sm)
 	sm->GUpdateStationKeys = false;
 
 #ifdef CONFIG_IEEE80211BE
-	for_each_sm_auth(sm, link_id)
-		sm->mld_links[link_id].wpa_auth->group->GKeyDoneStations--;
+	for_each_sm_auth(sm, link_id) {
+		struct wpa_authenticator *partner_auth =
+				sm->mld_links[link_id].wpa_auth;
+		struct wpa_group *partner_group =
+				wpa_select_vlan_wpa_group(partner_auth->group,
+							  sm->group->vlan_id);
+		partner_group->GKeyDoneStations--;
+	}
 #endif /* CONFIG_IEEE80211BE */
 }
 
@@ -5927,8 +5933,11 @@ static int wpa_group_update_sta(struct wpa_state_machine *sm, void *ctx)
 		if (!sm->mld_links[link_id].valid)
 			continue;
 		if (sm->mld_links[link_id].wpa_auth &&
-		    sm->mld_links[link_id].wpa_auth->group == ctx) {
-			group = sm->mld_links[link_id].wpa_auth->group;
+		    wpa_select_vlan_wpa_group(
+					      sm->mld_links[link_id].wpa_auth->group,
+					      sm->group->vlan_id) == ctx) {
+			group = wpa_select_vlan_wpa_group(sm->mld_links[link_id].wpa_auth->group,
+							  sm->group->vlan_id);
 			wpa_auth = sm->mld_links[link_id].wpa_auth;
 			break;
 		}
@@ -5976,8 +5985,14 @@ static int wpa_group_update_sta(struct wpa_state_machine *sm, void *ctx)
 
 	sm->group->GKeyDoneStations++;
 #ifdef CONFIG_IEEE80211BE
-	for_each_sm_auth(sm, link_id)
-		sm->mld_links[link_id].wpa_auth->group->GKeyDoneStations++;
+	for_each_sm_auth(sm, link_id) {
+		struct wpa_authenticator *partner_auth =
+					sm->mld_links[link_id].wpa_auth;
+		struct wpa_group *partner_group =
+					wpa_select_vlan_wpa_group(partner_auth->group,
+								  sm->group->vlan_id);
+		partner_group->GKeyDoneStations++;
+	}
 #endif /* CONFIG_IEEE80211BE */
 
 	sm->GUpdateStationKeys = true;
@@ -6534,12 +6549,16 @@ static void wpa_group_sm_step_links(struct wpa_state_machine *sm)
 
 	if (!sm || !sm->wpa_auth)
 		return;
-	wpa_group_sm_step(sm->wpa_auth, sm->wpa_auth->group);
+	wpa_group_sm_step(sm->wpa_auth, sm->group);
 
 #ifdef CONFIG_IEEE80211BE
 	for_each_sm_auth(sm, link_id) {
-		wpa_group_sm_step(sm->mld_links[link_id].wpa_auth,
-				  sm->mld_links[link_id].wpa_auth->group);
+		struct wpa_authenticator *partner_auth =
+					sm->mld_links[link_id].wpa_auth;
+		struct wpa_group *partner_group =
+					wpa_select_vlan_wpa_group(partner_auth->group,
+								  sm->group->vlan_id);
+		wpa_group_sm_step(partner_auth, partner_group);
 	}
 #endif /* CONFIG_IEEE80211BE */
 }
