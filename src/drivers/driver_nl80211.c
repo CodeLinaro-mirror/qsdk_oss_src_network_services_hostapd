0534c16a1e1bc2fe9867ae44f03a90d2d14daf36
Karthikeyan Kathirvel <kathirve@qti.qualcomm.com>
2025-12-10 01:49:18 -0800

    UPSTREAM:CP:hostapd: unable to set HE mode on 2.4Ghz
    Channel switch through hostapd_cli is not able to operate in HE mode in
    2.4Ghz. This is because since the he mode is not enabled for 2ghz the
    channel type is set to 20Mhz NO_HT.
    
    Enabled he for 2Ghz radio on 20Mhz bandwidth for channel switch.
    
    TAG: Channel_Switch
    
    clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/patches/l00-002-hostapd-fix-enabling-HE-thru-cli-in-2ghz.patch
    Gerrit: https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/3667691
    
    Change-Id: Ifd775c5a0657a50d95f01dbe76f93f1c08ab0fea
    Signed-off-by: Karthikeyan Kathirvel <kathirve@qti.qualcomm.com>
    Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -5365,7 +5365,7 @@ static int nl80211_put_freq_params(struct nl_msg *msg,
 		     nla_put_u32(msg, NL80211_ATTR_CENTER_FREQ2,
 				 freq->center_freq2)))
 			return -ENOBUFS;
-	} else if (freq->ht_enabled) {
+	} else if (freq->ht_enabled || ((freq->he_enabled || freq->eht_enabled) && is_24ghz)) {
 		enum nl80211_channel_type ct;
 
 		wpa_printf(MSG_DEBUG, "  * sec_channel_offset=%d",
