c99bfae8744fa846b6d76f69049ba261468d6d45
Pankaj Rawat <panraw@qti.qualcomm.com>
2025-12-10 15:17:39 +0530

    UPSTREAM:PROTOCOL:extn:Add new traffic type value for flow report vendor attribute
    Define new traffic type value for
    QCA_WLAN_VENDOR_ATTR_FLOW_STATS_TRAFFIC_TYPE attribute to
    detect livestream traffic.
    
    TAG: DOWNSTREAM
    
    Feature: VENDOR_CMD
    
    Patch-work: https://git.w1.fi/cgit/hostap/commit/?id=2f58e57fca48b0540d6b5315f7475c513d87788e
    Patch-dependency: None
    
    Change-Id: I2c562c08edc0795f764ee58978bf1e020c483293
    Signed-off-by: Pankaj Rawat <panraw@qti.qualcomm.com>

--- src/common/qca-vendor.h
+++ src/common/qca-vendor.h
@@ -19047,6 +19047,7 @@ enum qca_wlan_vendor_attr_ap_suspend {
  * @QCA_TRAFFIC_TYPE_INVALID: Invalid traffic type
  * @QCA_TRAFFIC_TYPE_BROWSING: Traffic type is browsing website
  * @QCA_TRAFFIC_TYPE_APERIODIC_BURSTS: Traffic type is aperiodic bursts
+ * @QCA_TRAFFIC_TYPE_LIVESTREAM: Traffic type is livestream
  */
 enum qca_traffic_type {
 	QCA_TRAFFIC_TYPE_STREAMING = 0,
@@ -19058,6 +19059,7 @@ enum qca_traffic_type {
 	QCA_TRAFFIC_TYPE_INVALID = 6,
 	QCA_TRAFFIC_TYPE_BROWSING = 7,
 	QCA_TRAFFIC_TYPE_APERIODIC_BURSTS = 8,
+	QCA_TRAFFIC_TYPE_LIVESTREAM = 9,
 };
 
 /**
