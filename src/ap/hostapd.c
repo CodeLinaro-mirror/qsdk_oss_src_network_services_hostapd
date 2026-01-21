greearb/hostap-ct

commit f406756eb45473d3aa3ed80213ef5890e230f7a2

AP MLD: Add helper function to get non-TX BSS based on MBSSID index
During ML Probe Request, a non-AP MLD might specify an AP MLD ID to let
the AP MLD know for which non-TX BSS it is requesting information.
Currently, while parsing the ML Probe Request frame, partner BSS
information is getting used to fetch the non-TX BSS requested by the
non-AP MLD.

However, relying on the partner information to fetch the non-TX BSS is
not correct in all the cases. Hence, remove fetching non-TX BSS from the
partner interface list and use MBSSID index received in the Probe
Request frame to get requested non-TX BSS data.

Address this by adding a new helper function to get the non-TX BSS from
the MBSSID index received in the Probe Request frame.

Signed-off-by: Rameshkumar Sundaram <rameshkumar.sundaram@oss.qualcomm.com>
Signed-off-by: Manish Dharanenthiran <manish.dharanenthiran@oss.qualcomm.com>

src/ap/hostapd.c
@@ -5273,3 +5273,13 @@ u16 hostapd_get_punct_bitmap(struct hostapd_data *hapd)
 
 	return punct_bitmap;
 }
+
+
+struct hostapd_data *
+hostapd_get_mbssid_bss_by_idx(struct hostapd_data *hapd, size_t idx)
+{
+	if (idx < hapd->iface->num_bss)
+		return hapd->iface->bss[idx];
+
+	return NULL;
+}

src/ap/hostapd.h
@@ -906,4 +906,7 @@ static inline bool ap_pmf_enabled(struct hostapd_bss_config *conf)
 enum oper_chan_width
 hostapd_chan_width_from_freq_params(struct hostapd_freq_params *freq_params);
 
+struct hostapd_data *
+hostapd_get_mbssid_bss_by_idx(struct hostapd_data *hapd, size_t idx);
+
 #endif /* HOSTAPD_H */

src/ap/beacon.c
@@ -1088,18 +1088,27 @@ static void hostapd_fill_probe_resp_ml_params(struct hostapd_data *hapd,
 		   "MLD: Got ML probe request with AP MLD ID %d for links %04x",
 		   mld_id, links);
 
+	/*
+	 * Set mld_ap if the ML probe request explicitly requested a specific
+	 * AP MLD ID.
+	 */
+	if (mld_id > 0) {
+		if (hapd == hostapd_mbssid_get_tx_bss(hapd)) {
+			hapd = hostapd_get_mbssid_bss_by_idx(hapd, mld_id);
+			if (!hapd) {
+				wpa_printf(MSG_INFO,
+					   "Ignore Probe Request from " MACSTR
+					   " since no matched non-TX BSS found for MBSSID Index %d",
+					   MAC2STR(mgmt->sa), mld_id);
+				goto fail;
+			}
+		}
+		params->mld_ap = hapd;
+	}
+
 	for_each_mld_link(link, hapd) {
 		struct mld_link_info *link_info;
 		u8 mld_link_id = link->mld_link_id;
-
-		/*
-		 * Set mld_ap iff the ML probe request explicitly
-		 * requested a specific MLD ID. In that case, the targeted
-		 * AP may have been a nontransmitted BSSID on the same
-		 * interface.
-		 */
-		if (mld_id != -1 && link->iface == hapd->iface)
-			params->mld_ap = link;
 
 		/* Never duplicate main Probe Response frame body */
 		if (link == hapd)
@@ -1118,7 +1127,7 @@ static void hostapd_fill_probe_resp_ml_params(struct hostapd_data *hapd,
 			   mld_link_id, link_info->resp_sta_profile_len);
 	}
 
-	if (mld_id != -1 && !params->mld_ap) {
+	if (mld_id > 0 && !params->mld_ap) {
 		wpa_printf(MSG_DEBUG,
 			   "MLD: No nontransmitted BSSID for MLD ID %d",
 			   mld_id);

CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit 9f4d164fd7a360a4a9ade6ca52133d17c9d7b26e

UPSTREAM:PROTOCOL:hostapd: add helper function to get max BSS for MBSSID
Add the helper function missed during patch to source directory
transition.

TAG: MBSSID

Patch-dependency: f406756eb454 ("AP MLD: Add helper function to get non-TX BSS based on MBSSID index")

Fixes: f406756eb454 ("AP MLD: Add helper function to get non-TX BSS
based on MBSSID index")
Change-Id: I295e5813c245ba1b8ece21e05c467872395eb79d
Signed-off-by: Rameshkumar Sundaram<rameshkumar.sundaram@oss.qualcomm.com>
Signed-off-by: Karthik <karthik.m@oss.qualcomm.com>
Signed-off-by: Yuvarani V <yuvarani.v@oss.qualcomm.com>
Signed-off-by: Aloka Dixit <aloka.dixit@oss.qualcomm.com>

src/ap/hostapd.c
@@ -8627,13 +8627,32 @@ u16 hostapd_get_punct_bitmap(struct hostapd_data *hapd)
 }
 
 
+size_t hostapd_get_mbssid_max_num_bss(struct hostapd_data *hapd)
+{
+	if (!hapd->iconf->mbssid || !hapd->iface)
+		return 0;
+
+	if (hapd->iconf->mbssid == MULTI_MBSSID_GROUP_ENABLED) {
+		if (!hapd->mbssid_group)
+			return 0;
+		return hapd->mbssid_group->num_bss;
+	} else {
+		return hapd->iface->num_bss;
+	}
+}
+
+
 struct hostapd_data *
 hostapd_get_mbssid_bss_by_idx(struct hostapd_data *hapd, size_t idx)
 {
-	if (idx < hapd->iface->num_bss)
-		return hapd->iface->bss[idx];
+	if (idx >= hostapd_get_mbssid_max_num_bss(hapd))
+		return NULL;
 
-	return NULL;
+	if (hapd->iconf->mbssid == MULTI_MBSSID_GROUP_ENABLED) {
+		return hostapd_get_multi_group_bss(hapd->mbssid_group, idx);
+	} else {
+		return hapd->iface->bss[idx];
+	}
 }
 
 /**

src/ap/hostapd.h
@@ -1124,6 +1124,7 @@ int hostapd_set_acl(struct hostapd_data *hapd);
 int hostapd_tx_bss_only(struct hostapd_data *hapd, const char *op_name);
 struct hostapd_data * hostapd_mbssid_get_tx_bss(struct hostapd_data *hapd);
 unsigned int hostapd_mbssid_get_bss_index(struct hostapd_data *hapd);
+size_t hostapd_get_mbssid_max_num_bss(struct hostapd_data *hapd);
 struct hostapd_data * hostapd_mld_get_link_bss(struct hostapd_data *hapd,
 					       u8 link_id);
 int hostapd_link_remove(struct hostapd_data *hapd, u32 count);