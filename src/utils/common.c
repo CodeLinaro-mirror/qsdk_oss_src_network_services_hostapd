greearb/hostap-ct

commit 56f7d76d745addac7666e8e5c0bd4ac84c902f5e

int_array: Add a function to check for equality
Add int_array_equal() to allow comparison of two int_array
instances. Return true if both arrays included the same set of integers.

Co-developed-by: Pooventhiran G <quic_pooventh@quicinc.com>
Signed-off-by: Pooventhiran G <quic_pooventh@quicinc.com>
Signed-off-by: Rohan Dutta <quic_drohan@quicinc.com>

src/utils/common.c
@@ -1003,6 +1003,28 @@ bool int_array_includes(const int *arr, int val)
 }
 
 
+bool int_array_equal(const int *a, const int *b)
+{
+	size_t alen, blen, i;
+
+	if (!a || !b)
+		return false;
+
+	alen = int_array_len(a);
+	blen = int_array_len(b);
+
+	if (alen != blen)
+		return false;
+
+	for (i = 0; i <= alen; i++) {
+		if (!int_array_includes(b, a[i]))
+			return false;
+	}
+
+	return true;
+}
+
+
 void str_clear_free(char *str)
 {
 	if (str) {

src/utils/common.h
@@ -595,6 +595,7 @@ void int_array_concat(int **res, const int *a);
 void int_array_sort_unique(int *a);
 void int_array_add_unique(int **res, int a);
 bool int_array_includes(const int *arr, int val);
+bool int_array_equal(const int *a, const int *b);
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 

CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit 3a8bc853ccd7f562f728312676e947dcf0af2623

UPSTREAM:PROTOCOL:hostapd:int_array: Only iterate existing elements in equality check
Fix the upper bound of the for loop to terminate before the final
element is reached. Without this fix, the loop would try to find the
trailing zero in the second array, resulting in the comparison to be
always false.

TAG: DOWNSTREAM

Feature: GENERAL

Patch-work: https://git.w1.fi/cgit/hostap/commit/?id=ef0cf9900b05123fe1bdc93f756992538a7fa562
Patch-dependency: None

Fixes: 56f7d76d745a ("int_array: Add a function to check for equality")
Change-Id: I76a07a588e79de814f66f595aeb424ba3acc6586
CC: Rohan Dutta <quic_drohan@quicinc.com>
Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>

src/utils/common.c
@@ -1016,7 +1016,7 @@ bool int_array_equal(const int *a, const int *b)
 	if (alen != blen)
 		return false;
 
-	for (i = 0; i <= alen; i++) {
+	for (i = 0; i < alen; i++) {
 		if (!int_array_includes(b, a[i]))
 			return false;
 	}