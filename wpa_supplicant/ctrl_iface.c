greearb/hostap-ct

commit 7c262f94ff80bc88d1051b66032757ec5d51f04d

PR: Control interface support to set and clear PR device identity context
These are for testing purposes and require CONFIG_TESTING_OPTIONS=y in
the build.

Signed-off-by: Peddolla Harshavardhan Reddy <peddolla@qti.qualcomm.com>

wpa_supplicant/ctrl_iface.c
@@ -11542,6 +11542,59 @@ static int wpas_ctrl_iface_pr_pasn_start(struct wpa_supplicant *wpa_s,
 
 
 #ifdef CONFIG_TESTING_OPTIONS
+
+#ifdef CONFIG_PR
+static int wpas_ctrl_iface_pr_set_dik_ctx(struct wpa_supplicant *wpa_s,
+					  char *cmd)
+{
+	int ret = -1;
+	bool own = false;
+	char *token, *context = NULL;
+	const char *password = NULL;
+	const u8 *pmk = NULL, *dik = NULL;
+	struct wpabuf *pmk_buf = NULL, *dik_buf = NULL;
+
+	while ((token = str_token(cmd, " ", &context))) {
+		if (os_strcmp(token, "self") == 0) {
+			own = true;
+			continue;
+		}
+
+		if (os_strncmp(token, "dik=", 4) == 0) {
+			dik_buf = wpabuf_parse_bin(token + 4);
+			if (!dik_buf)
+				goto fail;
+			dik = wpabuf_head_u8(dik_buf);
+			continue;
+		}
+
+		if (os_strncmp(token, "password=", 9) == 0) {
+			password = token + 9;
+			continue;
+		}
+
+		if (os_strncmp(token, "pmk=", 4) == 0) {
+			pmk_buf = wpabuf_parse_bin(token + 4);
+			if (!pmk_buf)
+				goto fail;
+			pmk = wpabuf_head_u8(pmk_buf);
+			continue;
+		}
+	}
+
+	if (!dik)
+		goto fail;
+
+	wpas_pr_set_dev_ik(wpa_s, dik, password, pmk, own);
+	ret = 0;
+fail:
+	wpabuf_clear_free(dik_buf);
+	wpabuf_clear_free(pmk_buf);
+	return ret;
+}
+#endif /* CONFIG_PR */
+
+
 static int wpas_ctrl_iface_pasn_driver(struct wpa_supplicant *wpa_s, char *cmd)
 {
 	char *token, *context = NULL;
@@ -11655,6 +11708,7 @@ out:
 
 	return ret;
 }
+
 #endif /* CONFIG_TESTING_OPTIONS */
 
 #endif /* CONFIG_PASN */
@@ -14079,6 +14133,13 @@ char * wpa_supplicant_ctrl_iface_process(struct wpa_supplicant *wpa_s,
 			reply_len = -1;
 #endif /* CONFIG_PR */
 #ifdef CONFIG_TESTING_OPTIONS
+#ifdef CONFIG_PR
+	} else if (os_strncmp(buf, "PR_SET_DIK_CONTEXT ", 19) == 0) {
+		if (wpas_ctrl_iface_pr_set_dik_ctx(wpa_s, buf + 19) < 0)
+			reply_len = -1;
+	} else if (os_strcmp(buf, "PR_CLEAR_DIK_CONTEXT") == 0) {
+		wpas_pr_clear_dev_iks(wpa_s);
+#endif /* CONFIG_PR */
 	} else if (os_strncmp(buf, "PASN_DRIVER ", 12) == 0) {
 		if (wpas_ctrl_iface_pasn_driver(wpa_s, buf + 12) < 0)
 			reply_len = -1;

CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit d1558c62b4ea8250ad4f09d7ceec7557d74c06c2

UPSTREAM:PROTOCOL:wpa_supplicant:PR: Move DIK set and clear commands out of testing scope
The commands PR_SET_DIK_CONTEXT and PR_CLEAR_DIK_CONTEXT should not
be under CONFIG_TESTING_OPTIONS since their usage is not limited to
testing only.

TAG: DOWNSTREAM

Feature: CONFIG

Patch-work: https://git.w1.fi/cgit/hostap/commit/?id=abaa2ca01648da7f360a2117b86adb8fb5f94257
Patch-dependency: None

Fixes: 7c262f94ff80 ("PR: Control interface support to set and clear PR device identity context")
Change-Id: I8a900f5428ecebf184b2b2260ece6f05a2754505
Signed-off-by: Peddolla Harshavardhan Reddy <peddolla@qti.qualcomm.com>

wpa_supplicant/ctrl_iface.c
@@ -11540,6 +11540,7 @@ static int wpas_ctrl_iface_pasn_deauthenticate(struct wpa_supplicant *wpa_s,
 
 
 #ifdef CONFIG_PR
+
 static int wpas_ctrl_iface_pr_pasn_start(struct wpa_supplicant *wpa_s,
 					 char *cmd)
 {
@@ -11590,12 +11591,8 @@ static int wpas_ctrl_iface_pr_pasn_start(struct wpa_supplicant *wpa_s,
 	return wpas_pr_initiate_pasn_auth(wpa_s, addr, freq, auth_mode, role,
 					  ranging_type, forced_pr_freq);
 }
-#endif /* CONFIG_PR */
 
 
-#ifdef CONFIG_TESTING_OPTIONS
-
-#ifdef CONFIG_PR
 static int wpas_ctrl_iface_pr_set_dik_ctx(struct wpa_supplicant *wpa_s,
 					  char *cmd)
 {
@@ -11644,9 +11641,11 @@ fail:
 	wpabuf_clear_free(pmk_buf);
 	return ret;
 }
+
 #endif /* CONFIG_PR */
 
 
+#ifdef CONFIG_TESTING_OPTIONS
 static int wpas_ctrl_iface_pasn_driver(struct wpa_supplicant *wpa_s, char *cmd)
 {
 	char *token, *context = NULL;
@@ -14290,15 +14289,13 @@ char * wpa_supplicant_ctrl_iface_process(struct wpa_supplicant *wpa_s,
 	} else if (os_strncmp(buf, "PR_PASN_START ", 14) == 0) {
 		if (wpas_ctrl_iface_pr_pasn_start(wpa_s, buf + 14) < 0)
 			reply_len = -1;
-#endif /* CONFIG_PR */
-#ifdef CONFIG_TESTING_OPTIONS
-#ifdef CONFIG_PR
 	} else if (os_strncmp(buf, "PR_SET_DIK_CONTEXT ", 19) == 0) {
 		if (wpas_ctrl_iface_pr_set_dik_ctx(wpa_s, buf + 19) < 0)
 			reply_len = -1;
 	} else if (os_strcmp(buf, "PR_CLEAR_DIK_CONTEXT") == 0) {
 		wpas_pr_clear_dev_iks(wpa_s);
 #endif /* CONFIG_PR */
+#ifdef CONFIG_TESTING_OPTIONS
 	} else if (os_strncmp(buf, "PASN_DRIVER ", 12) == 0) {
 		if (wpas_ctrl_iface_pasn_driver(wpa_s, buf + 12) < 0)
 			reply_len = -1;