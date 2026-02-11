CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit 9fc33fb5343e7eb16e9318c3194b4c845c018037

UPSTREAM:PROTOCOL: hostapd: Add definitions for IEEE80211BN UHR
Add support for generating and parsing the IEEE 802.11bn UHR Capabilities
element in hostapd. This includes defining the element structure,
advertising it in management frames, and extending the
parser to recognize and extract UHR capability data.

Add ieee802_11_uhr.c file for parsing UHR related APIs.

Patch-dependency: None
Patch-work: None

Change-Id: I85a95217b58dc992129e969e1c122c6341d2abaa
Signed-off-by: Diya Sati <dsati@qti.qualcomm.com>
Signed-off-by: Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>

src/common/ieee802_11_common.c
@@ -265,6 +265,10 @@ static int ieee802_11_parse_mle(const u8 *pos, size_t elen, size_t **total_len,
 		elems->prior_access_mle_len = elen;
 		*total_len = &elems->prior_access_mle_len;
 		break;
+	case WLAN_EID_EXT_UHR_CAPABILITIES:
+		elems->uhr_capabilities = pos;
+		elems->uhr_capabilities_len = elen;
+		break;
 	default:
 		if (show_errors) {
 			wpa_printf(MSG_MSGDUMP,
@@ -1003,6 +1007,10 @@ void ieee802_11_elems_clear_ext_ids(struct ieee802_11_elems *elems,
 		case WLAN_EID_EXT_EHT_OPERATION:
 			elems->eht_operation = NULL;
 			elems->eht_operation_len = 0;
+			break;
+		case WLAN_EID_EXT_UHR_CAPABILITIES:
+			elems->uhr_capabilities = NULL;
+			elems->uhr_capabilities_len = 0;
 			break;
 		}
 	}

hostapd/Makefile
@@ -385,6 +385,7 @@ endif
 ifdef CONFIG_IEEE80211BN
 CONFIG_IEEE80211BE=y
 CFLAGS += -DCONFIG_IEEE80211BN
+OBJS += ../src/ap/ieee802_11_uhr.o
 endif
 
 ifdef CONFIG_IEEE80211BE

src/ap/ieee802_11.h
@@ -509,6 +509,8 @@ u8 * hostapd_eid_eht_capab(struct hostapd_data *hapd, u8 *eid,
 			   enum ieee80211_op_mode opmode);
 u8 * hostapd_eid_eht_operation(struct hostapd_data *hapd, u8 *eid);
 bool eht_mu_mask_valid(u8 mask);
+u8 * hostapd_eid_uhr_capab(struct hostapd_data *hapd, u8 *eid,
+			   enum ieee80211_op_mode opmode);
 u16 copy_sta_eht_capab(struct hostapd_data *hapd, struct sta_info *sta,
 		       enum ieee80211_op_mode opmode,
 		       const u8 *he_capab, size_t he_capab_len,

src/ap/ieee802_11_uhr.c
@@ -1,0 +1,44 @@
+
+/*
+ * hostapd / IEEE 802.11bn UHR
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include "utils/includes.h"
+#include "utils/common.h"
+#include "hostapd.h"
+#include "sta_info.h"
+#include "ieee802_11.h"
+
+
+u8 * hostapd_eid_uhr_capab(struct hostapd_data *hapd, u8 *eid,
+			    enum ieee80211_op_mode opmode)
+{
+	struct hostapd_hw_modes *mode;
+	struct uhr_capabilities *uhr_cap;
+	struct ieee80211_uhr_capabilities *cap;
+	u8 *pos = eid, *length_pos;
+
+	mode = hapd->iface->current_mode;
+	if (!mode)
+		return eid;
+
+	uhr_cap = &mode->uhr_capab[opmode];
+	if (!uhr_cap->uhr_supported)
+		return eid;
+
+	*pos++ = WLAN_EID_EXTENSION;
+	length_pos = pos++;
+	*pos++ = WLAN_EID_EXT_UHR_CAPABILITIES;
+
+	cap = (struct ieee80211_uhr_capabilities *)pos;
+	os_memset(cap, 0, sizeof(*cap));
+	os_memcpy(cap->mac_cap, uhr_cap->mac_cap, sizeof(cap->mac_cap));
+	os_memcpy(cap->phy_cap, uhr_cap->phy_cap, sizeof(cap->phy_cap));
+	pos += sizeof(struct ieee80211_uhr_capabilities);
+
+	*length_pos = pos - (eid + 2);
+	return pos;
+}

src/common/ieee802_11_common.h
@@ -117,6 +117,7 @@ struct ieee802_11_elems {
 	const u8 *pasn_params;
 	const u8 *eht_capabilities;
 	const u8 *eht_operation;
+	const u8 *uhr_capabilities;
 	const u8 *basic_mle;
 	const u8 *probe_req_mle;
 	const u8 *reconf_mle;
@@ -189,6 +190,7 @@ struct ieee802_11_elems {
 	u8 sae_pk_len;
 	u8 pasn_params_len;
 	u8 eht_capabilities_len;
+	u8 uhr_capabilities_len;
 	u8 eht_operation_len;
 	size_t basic_mle_len;
 	size_t probe_req_mle_len;

src/common/ieee802_11_defs.h
@@ -530,6 +530,7 @@
 #define WLAN_EID_EXT_BANDWIDTH_INDICATION 135
 #define WLAN_EID_EXT_KNOWN_STA_IDENTIFICATION 136
 #define WLAN_EID_EXT_PASN_ENCRYPTED_DATA 140
+#define WLAN_EID_EXT_UHR_CAPABILITIES 152
 
 /* Extended Capabilities field */
 #define WLAN_EXT_CAPAB_20_40_COEX 0

wpa_supplicant/Makefile
@@ -1058,6 +1058,9 @@ ifdef CONFIG_IEEE80211BE
 OBJS += ../src/ap/ieee802_11_eht.o
 OBJS += ../src/ap/ttlm.o
 endif
+ifdef CONFIG_IEEE80211BN
+OBJS += ../src/ap/ieee802_11_uhr.o
+endif
 OBJS += ../src/ap/dscp_policy.o
 ifdef CONFIG_WNM_AP
 CFLAGS += -DCONFIG_WNM_AP

commit 9dab4641f0bff23897f02324a163c9045f031d16

UPSTREAM:PROTOCOL: hostapd: Parse the uhr_capabilities in ieee802_11_parse_extension()
Currently the uhr_capabilities parsed in ieee802_11_parse_mle() which is
wrong hence facing UHR connectivity issue.

Fix it by parsing the uhr_capabilities in ieee802_11_parse_extension().

Fixes: 9fc33fb5343e ("UPSTREAM:PROTOCOL: hostapd: Add definitions for IEEE80211BN UHR")

Patch-dependency: None

Change-Id: I963df49ef1eda26f479d1958a1db64f1e5d6d9ef
Signed-off-by: Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>

src/common/ieee802_11_common.c
@@ -268,14 +268,6 @@ static int ieee802_11_parse_mle(const u8 *pos, size_t elen, size_t **total_len,
 		elems->prior_access_mle_len = elen;
 		*total_len = &elems->prior_access_mle_len;
 		break;
-	case WLAN_EID_EXT_UHR_CAPABILITIES:
-		elems->uhr_capabilities = pos;
-		elems->uhr_capabilities_len = elen;
-		break;
-	case WLAN_EID_EXT_UHR_OPERATION:
-		elems->uhr_operation = pos;
-		elems->uhr_operation_len = elen;
-		break;
 	default:
 		if (show_errors) {
 			wpa_printf(MSG_MSGDUMP,
@@ -436,6 +428,14 @@ static int ieee802_11_parse_extension(const u8 *pos, size_t elen,
 			break;
 		elems->eht_operation = pos;
 		elems->eht_operation_len = elen;
+		break;
+	case WLAN_EID_EXT_UHR_CAPABILITIES:
+		elems->uhr_capabilities = pos;
+		elems->uhr_capabilities_len = elen;
+		break;
+	case WLAN_EID_EXT_UHR_OPERATION:
+		elems->uhr_operation = pos;
+		elems->uhr_operation_len = elen;
 		break;
 	case WLAN_EID_EXT_MULTI_LINK:
 		if (elen < 2)