d7ca1cf71a2f60204a06bddc6ea37b804bd41fe4
Ainy Kumari <ainy.kumari@oss.qualcomm.com>
2026-02-17 23:02:06 +0530

    UPSTREAM:HOSTAPD:hostapd: EPPKE: Retrieve PMKID ANonce from (Re)Association Response frame
    Extract the Nonce element from the encrypted (Re)Association Response
    frame and cache it as ANonce for later PMKID privacy derivation.
    
    Topic: eppke-wds-sta-ml
    
    Upstream-commit:
    https://git.w1.fi/cgit/hostap/commit/?id=3a081180e881aa5533d59172c87fca5594ad94bf
    
    Patch-dependency: None
    
    CRs-Fixed: 4622393
    Change-Id: Ic3625b9bbfe8465e9049bb98eb7099d31ad277b6
    Signed-off-by: Priyadharshini Gowthaman <pgowtham@qti.qualcomm.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -4286,6 +4286,16 @@ static void wpas_parse_connection_info(struct wpa_supplicant *wpa_s,
 	max_nss_tx_resp = get_max_nss_capability(&resp_elems, 0);
 	wpa_s->connection_max_nss_tx = MIN(max_nss_tx_resp, max_nss_tx_req);
 
+#ifdef CONFIG_PMKSA_PRIVACY
+	if (wpa_s->assoc_resp_encrypted && resp_elems.nonce) {
+		os_memcpy(wpa_s->pmkid_anonce, resp_elems.nonce, NONCE_LEN);
+		wpa_s->pmkid_anonce_set = true;
+		wpa_hexdump(MSG_DEBUG,
+			    "PMKID privacy: ANonce in Assoc Response",
+			    wpa_s->pmkid_anonce, NONCE_LEN);
+	}
+#endif /* CONFIG_PMKSA_PRIVACY */
+
 	sta_supported_chan_width = get_supported_channel_width(&req_elems);
 	ap_operation_chan_width = get_operation_channel_width(&resp_elems);
 	if (wpa_s->connection_vht || wpa_s->connection_he ||
