4fe4ae3be3500dfe033354bbbb2b2c9b06f52deb
Hari G <hag@qti.qualcomm.com>
2026-02-27 14:43:54 +0530

    hostapd: Allow an EPP station to reconnect without a deauthentication/disassociation.
    An EPP station that is already in the authorized
    state can attempt re-association without a
    deauthentication or disassociation. In this case,
    do not reject the EPPKE authentication frame 1
    from the station.
    
    Change-Id: I6084df4b5e86d8cea0652ee146cbe4dbee844c01
    Signed-off-by: Hari G <hag@qti.qualcomm.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -3401,7 +3401,7 @@ static void handle_auth_pasn(struct hostapd_data *hapd, struct sta_info *sta,
 		   MAC2STR(sta->addr));
 
 	if (trans_seq == 1) {
-		if (sta->pasn) {
+		if (sta->pasn && sta->auth_alg != WLAN_AUTH_EPPKE) {
 			wpa_printf(MSG_DEBUG,
 				   "PASN: Not expecting transaction == 1");
 			return;
