40079c69e635b1af2cdcaa03b04fc8e178a35ea7
Sai Pratyusha Magam <sai.magam@oss.qualcomm.com>
2026-04-08 12:36:28 +0530

    PASN: Supported groups element in Autentication frame 2
    Include a Supported groups information element with the list
    of groups AP supports, in PASN authentication frame 2 when
    the processing status of the authentication frame 1 is success
    and UNSUPPORTED_FINITE_CYCLIC_GROUP
    
    Upstream-commit: https://git.w1.fi/cgit/hostap/commit/?id=f8c4b54227d019e8b3f8a8e750fb213248d81ae5
    
    Change-Id: Ieedb7d63b8df9b23908391585f3100448d523f76
    Signed-off-by: Sai Pratyusha Magam <sai.magam@oss.qualcomm.com>

--- src/pasn/pasn_responder.c
+++ src/pasn/pasn_responder.c
@@ -594,6 +594,9 @@ int handle_auth_pasn_resp(struct pasn_data *pasn, const u8 *own_addr,
 				   pasn->is_ml_peer ? pasn->reply_addr : pasn->peer_addr,
 				   2, status, pasn->auth_alg == WLAN_AUTH_EPPKE);
 
+	if (status == WLAN_STATUS_FINITE_CYCLIC_GROUP_NOT_SUPPORTED)
+		wpa_pasn_add_own_supported_groups(buf, pasn->pasn_groups);
+
 	if (status != WLAN_STATUS_SUCCESS)
 		goto done;
 
@@ -656,6 +659,8 @@ int handle_auth_pasn_resp(struct pasn_data *pasn, const u8 *own_addr,
 		pasn->prepare_data_element(pasn->cb_ctx, peer_addr);
 
 	wpa_pasn_add_extra_ies(buf, pasn->extra_ies, pasn->extra_ies_len);
+
+	wpa_pasn_add_own_supported_groups(buf, pasn->pasn_groups);
 
 #ifdef CONFIG_ENC_ASSOC
 	if (pasn->auth_alg == WLAN_AUTH_EPPKE && pasn->is_ml_peer) {
