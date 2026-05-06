CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit fda2801838d5bf11eb78d13fced4e55f048a56cb

UPSTREAM:PROTOCOL:hostapd: Add intelligence color choose in CCA
Gather color information of the neighbor APs and choose available
free color after go through the neighbor colors to minimise the color collision

TAG: BSS Color

clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/patches/f00-007-bss_coloring-add-intelligence-color-choose-in-CCA.patch

Gerrit: https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/3361954

Change-Id: I53743457b3054b8a09f85302593d30e0dda5e6cd
Signed-off-by: Karthikeyan Periyasamy <periyasa@qti.qualcomm.com>
Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

src/ap/hostapd.c
@@ -4964,6 +4964,7 @@ static void hostapd_switch_color_timeout_handler(void *eloop_data,
 	os_time_t delta_t;
 	unsigned int b;
 	int i, r;
+	u64 neighbor_color;
 
 	 /* CCA can be triggered once the handler constantly receives
 	  * color collision events to for at least
@@ -4973,12 +4974,16 @@ static void hostapd_switch_color_timeout_handler(void *eloop_data,
 	if (delta_t < DOT11BSS_COLOR_COLLISION_AP_PERIOD)
 		return;
 
-	r = os_random() % HE_OPERATION_BSS_COLOR_MAX;
-	for (i = 0; i < HE_OPERATION_BSS_COLOR_MAX; i++) {
-		if (r && !(hapd->color_collision_bitmap & (1ULL << r)))
-			break;
+	neighbor_color = ap_list_get_color(hapd->iface);
+	 neighbor_color |= hapd->color_collision_bitmap;
 
-		r = (r + 1) % HE_OPERATION_BSS_COLOR_MAX;
+	 r = os_random() % HE_OPERATION_BSS_COLOR_MAX - 1;
+	 r++;
+	 for (i = 1; i < HE_OPERATION_BSS_COLOR_MAX; i++) {
+		 if ((neighbor_color & (1 << r)) == 0)
+			break;
+		r = r % HE_OPERATION_BSS_COLOR_MAX - 1;
+		r++;
 	}
 
 	if (i == HE_OPERATION_BSS_COLOR_MAX) {

src/ap/ap_list.c
@@ -174,6 +174,7 @@ void ap_list_process_beacon(struct hostapd_iface *iface,
 	struct ap_info *ap;
 	int new_ap = 0;
 	int set_beacon = 0;
+	u32 he_operation;
 
 	if (iface->conf->ap_table_max_size < 1)
 		return;
@@ -209,6 +210,17 @@ void ap_list_process_beacon(struct hostapd_iface *iface,
 		ap->ht_support = 1;
 	else
 		ap->ht_support = 0;
+
+	 if (iface->conf->ieee80211ax &&
+	     elems->he_operation) {
+		 he_operation = *(u32 *)elems->he_operation;
+
+		 if (!(he_operation & HE_OPERATION_BSS_COLOR_DISABLED))
+			 ap->color = (he_operation & HE_OPERATION_BSS_COLOR_MASK) >>
+				     HE_OPERATION_BSS_COLOR_OFFSET;
+		 else
+			 ap->color = 0;
+	}
 
 	os_get_reltime(&ap->last_beacon);
 
@@ -295,6 +307,21 @@ void ap_list_timer(struct hostapd_iface *iface)
 		ieee802_11_update_beacons(iface);
 }
 
+u64 ap_list_get_color(struct hostapd_iface *iface)
+{
+	u64 used_color_bitmap = 0;
+	struct ap_info *ap;
+
+	if (!iface->ap_list)
+		return used_color_bitmap;
+
+	ap = iface->ap_list;
+	while (ap != NULL) {
+		used_color_bitmap |= (u64)1 << ap->color;
+		ap = ap->next;
+	}
+	return used_color_bitmap;
+}
 
 int ap_list_init(struct hostapd_iface *iface)
 {

src/ap/ap_list.h
@@ -25,6 +25,7 @@ struct ap_info {
 	int channel;
 
 	int ht_support;
+	u8 color;
 
 	struct os_reltime last_beacon;
 };
@@ -40,6 +41,7 @@ void ap_list_process_beacon(struct hostapd_iface *iface,
 int ap_list_init(struct hostapd_iface *iface);
 void ap_list_deinit(struct hostapd_iface *iface);
 void ap_list_timer(struct hostapd_iface *iface);
+u64 ap_list_get_color(struct hostapd_iface *iface);
 #else /* NEED_AP_MLME */
 static inline int ap_list_init(struct hostapd_iface *iface)
 {
@@ -52,6 +54,11 @@ static inline void ap_list_deinit(struct hostapd_iface *iface)
 
 static inline void ap_list_timer(struct hostapd_iface *iface)
 {
+}
+
+static u64 ap_list_get_color(struct hostapd_iface *iface)
+{
+	return 0;
 }
 #endif /* NEED_AP_MLME */
 

commit 146c389835daf61667b594435d6e88c9fcb60906

UPSTREAM:HOSTAPD: Revert to upstream implementation to fix BSS color collision issues
When color collisions occur, the logic for selecting a new BSS color
fails to evaluate all available options in the color list, leading to
failure or delays in selecting a suitable new color. In addition,
left-shifting a 32-bit integer for use in a 64-bit color bitmap can
lead to undefined behavior and crashes.
Fix the color generation logic by reverting to the upstream implementation.

This change was introduced earlier through https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/6343954,
but it was reverted during the package upgrade

Fixes: fda2801838d5 ("UPSTREAM:PROTOCOL:hostapd: Add intelligence color choose in CCA")
Fixes: 6121e97d7248 ("hostapd: add support for 11ax bss color")

Patch-dependency: f00-007-bss_coloring-add-intelligence-color-choose-in-CCA.patch

Change-Id: I1c33e2d650c65f634d36004e706b6506cfcaeb52
Signed-off-by: Suraj P Kizhakkethil <suraj.kizhakkethil@oss.qualcomm.com>

src/ap/hostapd.c
@@ -7956,13 +7956,11 @@ void hostapd_switch_color_timeout_handler(void *eloop_data,
 	if (!hapd->no_free_color)
 		 neighbor_color |= hapd->color_collision_bitmap;
 
-	 r = os_random() % HE_OPERATION_BSS_COLOR_MAX - 1;
-	 r++;
-	 for (i = 1; i < HE_OPERATION_BSS_COLOR_MAX; i++) {
-		 if ((neighbor_color & (1 << r)) == 0)
+	r = os_random() % HE_OPERATION_BSS_COLOR_MAX;
+	for (i = 0; i < HE_OPERATION_BSS_COLOR_MAX; i++) {
+		if (r && !(neighbor_color & (1ULL << r)))
 			break;
-		r = r % HE_OPERATION_BSS_COLOR_MAX - 1;
-		r++;
+		r = (r + 1) % HE_OPERATION_BSS_COLOR_MAX;
 	}
 
 	if (i == HE_OPERATION_BSS_COLOR_MAX) {