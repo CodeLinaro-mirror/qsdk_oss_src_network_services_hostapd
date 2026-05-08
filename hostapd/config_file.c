c8b1c89e2f4292b7bfe3be6c25b0b170433e506f
Harshitha Prem <hprem@qti.qualcomm.com>
2026-05-08 15:00:52 +0530

    UPSTREAM:PROTOCOL:hostapd: UHR: configure enhanced critical update intervals
    As per IEEE P802.11bn/D1.4, section 37.30 Enhanced BSS parameter
    critical update procedure uses 2 configurable intervals:
    - advance notification interval
    - update indication in TIM interval
    
    Implement configuration options for these intervals with the following
    default values:
            advance notification interval - 7 number of TBTTs.
            update indication in TIM interval - 10 number of TBTTs.
    
    These intervals are encoded into the UHR MAC Capabilities element
    (mac_cap bytes 3-4) and passed to the driver via wpa_driver_ap_params
    along with the full UHR capabilities element.
    
    Patch-dependency: Ie839bb18ad2 ("UPSTREAM:PROTOCOL: hostapd: Add configs for IEEE 802.11bn")
    
    Change-Id: I31f393a1484606ec70ee41ae1f650e37fcdad4b3
    Signed-off-by: Harshitha Prem <hprem@qti.qualcomm.com>

--- hostapd/config_file.c
+++ hostapd/config_file.c
@@ -6129,6 +6129,26 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 		}
 		bss->dps_assist = val;
 #endif /* CONFIG_IEEE80211BN */
+#ifdef CONFIG_IEEE80211BN
+	} else if (os_strcmp(buf, "uhr_adv_notification_interval") == 0) {
+		int val = atoi(pos);
+		if (val < 1 || val > 7) {
+			wpa_printf(MSG_ERROR,
+				   "Line %d: uhr_adv_notification_interval must be 1-7",
+				   line);
+			return 1;
+		}
+		bss->uhr_params_update.adv_notification_interval = (u8) val;
+	} else if (os_strcmp(buf, "uhr_update_in_tim_interval") == 0) {
+		int val = atoi(pos);
+		if (val < 1 || val > 31) {
+			wpa_printf(MSG_ERROR,
+				   "Line %d: uhr_update_in_tim_interval must be 1-31",
+				   line);
+			return 1;
+		}
+		bss->uhr_params_update.update_in_tim_interval = (u8) val;
+#endif /* CONFIG_IEEE80211BN */
 
 	} else if (os_strcmp(buf, "enable_dscp_policy_capa") == 0) {
 		bss->enable_dscp_policy_capa = atoi(pos);
--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -2578,6 +2578,12 @@ struct wpa_driver_ap_params {
 
 #ifdef CONFIG_IEEE80211BN
 	struct wpa_smd_params smd;
+
+	/**
+	 * uhr_cap - Full UHR capabilities element, includes the extended
+	 * element header
+	 */
+	u8 *uhr_cap;
 #endif /* CONFIG_IEEE80211BN */
 
 	/* Num common parameter to add in qca_vendor command */
