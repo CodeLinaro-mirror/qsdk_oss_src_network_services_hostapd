8ba4fb823802e4f633e06cc7e85fc6bb17499623
Pooja TC <ptc@qti.qualcomm.com>
2026-06-11 15:19:54 +0530

    QSDK:CP:hostapd: Validate BSS count for EMA
    When EMA is enabled, bringing up SLO/MLO with the maximum
    number of BSS (16) per radio is currently not supported. This
    is because the beacon management frame size exceeds the
    allocated limit of 2000 bytes, leading to BSS bring-up
    failure.
    
    This issue occurs due to the increased number of Information
    Elements (IEs) in Tx BSS and Non-Tx BSS profiles, which enlarges
    the beacon and probe response frame size.
    
    To address this issue, add a sanity check in
    hostapd_mbssid_setup_bss() to validate num_bss for EMA enabled
    case.
    
    TAG: MBSSID
    
    Patch-dependency: I2ffa3a6be0f ("QSDK:CP:hostapd-extn: Sanitize BSS
    count for EMA")
    
    Patch-work None
    
    Change-Id: I5da8b441d711b43cc3744b44502155fcbcf3fcdf
    Signed-off-by: Pooja TC <ptc@qti.qualcomm.com>

--- src/ap/hostapd.c
+++ src/ap/hostapd.c
@@ -5904,14 +5904,14 @@ static int hostapd_multi_mbssid_add_bss(struct hostapd_data *hapd)
 
 	addr = hostapd_addr_to_u64(hapd->own_addr);
 
-	if (hapd->iconf->mbssid == MULTI_MBSSID_GROUP_ENABLED) {
-
 #ifdef CONFIG_QCN_EXTN
-		if (hostapd_validate_mbssid_group_size_extn(hapd)) {
-			wpa_printf(MSG_ERROR, "Invalid group size");
-			return -1;
-		}
+	if (hostapd_validate_mbssid_configuration_extn(hapd)) {
+		wpa_printf(MSG_ERROR, "Invalid MBSSID configuration");
+		return -1;
+	}
 #endif
+
+	if (hapd->iconf->mbssid == MULTI_MBSSID_GROUP_ENABLED) {
 		if (!multi_mbssid->group) {
 			multi_mbssid->num_mbssid_groups = mbssid_max_interfaces /
 							  iface->conf->group_size;
