CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit 6992ff6adb60132abe9ad988c963e6480cd48a73

UPSTREAM:PROTOCOL: hostapd: add support to use bitmap to get link ids
Currently, link IDs are assigned based on a counter
stored in the hostapd MLD structure. With ML Reconfig
Link Removal, any link can be removed and, when re-added,
is assigned a new link ID. This process can eventually exhaust
all available link IDs.

To address this issue, use a bitmap instead of a counter.
A value of 1 at a given index indicates that the link ID
is available, while a value of 0 means it is currently
in use. This method simplifies tracking link IDs,
allowing them to be reused when necessary.
Also, retain the link ID of the disabled interface,
to ensure that the disabled interface link ID is not
utilized during dynamic link addition.

TAG: ML_RECONFIG

clo-patch-file: https://git.codelinaro.org/clo/qsdk/oss/system/feeds/wlan-hostapd/-/blob/win.wlan_host_opensource.4.0.r30/patches/r49-UPSTREAM-PROTOCOL-hostapd-add-support-to-use-bitmap-.patch
Gerrit: https://review-android.quicinc.com/c/oss/system/feeds/wlan-hostapd/+/5650483

Patch-dependency:
r32-005-hostapd-Handle-link-reconfig-event-started-and-compl.patch
r44-001-hostapd-ML-reconfiguration-link-addition-support.patch

Patch-work: None

Change-Id: Ide48cf8cc683b75f2e6a56ddb079da062c333057
Signed-off-by: Yuvarani V <quic_yuvarani@quicinc.com>
Signed-off-by: Roopni Devanathan <rdevanat@qti.qualcomm.com>

src/ap/hostapd.c
@@ -3481,11 +3481,18 @@ struct hostapd_iface * hostapd_alloc_iface(void)
 
 
 #ifdef CONFIG_IEEE80211BE
-static void hostapd_bss_alloc_link_id(struct hostapd_data *hapd)
+static int hostapd_bss_alloc_link_id(struct hostapd_data *hapd)
 {
-	hapd->mld_link_id = hapd->mld->next_link_id++;
+	/* All links are exhausted */
+	if (!hapd->mld->free_links)
+		return -1;
+
+	hapd->mld_link_id = ffs(hapd->mld->free_links) - 1;
+	hapd->mld->free_links &= ~BIT(hapd->mld_link_id);
 	wpa_printf(MSG_DEBUG, "AP MLD: %s: Link ID %d assigned.",
 		   hapd->mld->name, hapd->mld_link_id);
+
+	return 0;
 }
 #endif /* CONFIG_IEEE80211BE */
 
@@ -3515,7 +3522,8 @@ void hostapd_bss_setup_multi_link(struct hostapd_data *hapd,
 
 		hapd->mld = mld;
 		hostapd_mld_ref_inc(mld);
-		hostapd_bss_alloc_link_id(hapd);
+		if (hostapd_bss_alloc_link_id(hapd))
+			goto fail;
 		break;
 	}
 
@@ -3528,6 +3536,7 @@ void hostapd_bss_setup_multi_link(struct hostapd_data *hapd,
 
 	os_strlcpy(mld->name, conf->iface, sizeof(conf->iface));
 	dl_list_init(&mld->links);
+	mld->free_links = 0x7FFF;
 	mld->ctrl_sock = -1;
 	if (hapd->conf->ctrl_interface)
 		mld->ctrl_interface = os_strdup(hapd->conf->ctrl_interface);
@@ -3541,7 +3550,8 @@ void hostapd_bss_setup_multi_link(struct hostapd_data *hapd,
 
 	hapd->mld = mld;
 	hostapd_mld_ref_inc(mld);
-	hostapd_bss_alloc_link_id(hapd);
+	if (hostapd_bss_alloc_link_id(hapd))
+		goto fail;
 
 	all_mld = os_realloc_array(interfaces->mld, interfaces->mld_count + 1,
 				   sizeof(struct hostapd_mld *));
@@ -4197,6 +4207,13 @@ int hostapd_disable_iface(struct hostapd_iface *hapd_iface)
 		hostapd_bss_deinit_no_free(hapd);
 		hostapd_bss_link_deinit(hapd);
 		hostapd_free_hapd_data(hapd);
+#ifdef CONFIG_IEEE80211BE
+	/* Retain the link ID to ensure that the disabled interface link ID is not
+	 * utilized during dynamic link addition.
+	 */
+		if (hapd->mld)
+			hapd->mld->free_links &= ~BIT(hapd->mld_link_id);
+#endif /* CONFIG_IEEE80211BE */
 	}
 
 	hostapd_deinit_driver(hapd_iface->bss[0]->driver,
@@ -5913,6 +5930,7 @@ int hostapd_mld_remove_link(struct hostapd_data *hapd)
 		return -1;
 
 	dl_list_del(&hapd->link);
+	mld->free_links |= BIT(hapd->mld_link_id);
 	mld->num_links--;
 
 	wpa_printf(MSG_DEBUG, "AP MLD %s: Link ID %d removed. num_links: %d",

src/ap/hostapd.h
@@ -605,7 +605,7 @@ struct hostapd_ft_over_ds_ml_sta_entry {
 struct hostapd_mld {
 	char name[IFNAMSIZ + 1];
 	u8 mld_addr[ETH_ALEN];
-	u8 next_link_id;
+	u16 free_links;
 	u8 num_links;
 	/* Number of hostapd_data (hapd) referencing this. num_links cannot be
 	 * used since num_links can go to 0 even when a BSS is disabled and

commit d8de9e3926b3c96bdc1f279cf8484c7b8e28d3e4

UPSTREAM:PROTOCOL:hostapd: Fix UAF of existing MLD in setup fail path
hostapd_bss_setup_multi_link() uses a single fail: label to clean up
from two structurally different error cases:

Case 1 (existing MLD reuse): Search interfaces->mld[] by name, find a
match, call hostapd_mld_ref_inc(), then reach fail: on a subsequent
error. Here, mld points to an entry that is still tracked inside
interfaces->mld[]. The fail: label calls os_free(mld), freeing the
struct while leaving interfaces->mld[i] as a dangling pointer.

Case 2 (new MLD allocation): Allocate a new struct via os_zalloc().
If any initialisation step fails, reach fail: and call os_free(mld).
This is correct because the new struct has not yet been inserted into
interfaces->mld[].

Fix Case 1 to handle its error inline: call hostapd_mld_ref_dec() to
undo the ref_inc, clear hapd->mld, and return without reaching the
fail: label. The fail: label now handles only Case 2, where
os_free(mld) on the untracked new struct is correct.

Tag: ML_RECONFIG
Fixes: 6992ff6adb60 ("hostapd: add support to use bitmap to get link ids")
Patch-dependency: 6992ff6adb60 ("hostapd: add support to use bitmap to get link ids")

Change-Id: I12ed1b054981f33ae9af08ae4798a9ebf76dfb41
Signed-off-by: Govindaraj Saminathan <gsaminat@qti.qualcomm.com>

src/ap/hostapd.c
@@ -5700,8 +5700,11 @@ void hostapd_bss_setup_multi_link(struct hostapd_data *hapd,
 
 		hapd->mld = mld;
 		hostapd_mld_ref_inc(mld);
-		if (hostapd_bss_alloc_link_id(hapd))
-			goto fail;
+		if (hostapd_bss_alloc_link_id(hapd)) {
+			hostapd_mld_ref_dec(mld);
+			hapd->mld = NULL;
+			return;
+		}
 		break;
 	}
 