041933c109c8bb5b11a84d26a3ec30676afcbaeb
Adil Saeed Musthafa <adilm@qti.qualcomm.com>
2026-06-17 23:42:56 -0700

    UPSTREAM:HOSTAPD:Hostapd-plugin: Pass radio hw_idx for non-MLO BSS
    In hostapd-plugin outbound APIs, pass the radio hw_idx for non-MLO BSS
    instead of -1.
    
    Patch-dependency: I63231a89958997ac83297dc6d9166de5353e51f2
    
    Change-Id: Ib9b0c7f891c056dd0f4ea4396eb1a048d0dfbe0c
    Signed-off-by: Adil Saeed Musthafa <adilm@qti.qualcomm.com>

--- src/hostapd_if/hostapd_if.c
+++ src/hostapd_if/hostapd_if.c
@@ -581,7 +581,7 @@ hostapd_if_notify_auth(struct hostapd_data *hapd,
 	ctx_req.data.auth_req.auth_alg = auth_alg;
 
 	/*
-	 * rx_link_id selection: 0 for non-MLD, otherwise
+	 * rx_link_id selection: hw_idx for non-MLD, otherwise
 	 * hapd->mld_link_id
 	 */
 #ifdef CONFIG_IEEE80211BE
@@ -589,7 +589,7 @@ hostapd_if_notify_auth(struct hostapd_data *hapd,
 		ctx_req.rx_link_id = hapd->mld_link_id;
 	else
 #endif /* CONFIG_IEEE80211BE */
-		ctx_req.rx_link_id = 0;
+		ctx_req.rx_link_id = hapd->iface->current_hw_info->hw_idx;
 
 	/*
 	 * Associated link id and per-link peer MAC
@@ -649,7 +649,13 @@ hostapd_if_notify_remote_auth(struct hostapd_data *hapd, uint8_t *sta_mac,
 	os_memset(&ctx_req, 0, sizeof(ctx_req));
 	ctx_req.status_code = status_code;
 
-	ctx_req.rx_link_id = hapd->mld_link_id;
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->conf && hapd->conf->mld_ap)
+		ctx_req.rx_link_id = hapd->mld_link_id;
+	else
+#endif /* CONFIG_IEEE80211BE */
+		ctx_req.rx_link_id = hapd->iface->current_hw_info->hw_idx;
+
 	ctx_req.data.remote_auth_req.is_ml_sta = is_ml;
 
 #ifdef HOSTAPD_EXTERNAL_PLUGIN
@@ -786,7 +792,7 @@ hostapd_if_notify_assoc(struct hostapd_data *hapd,
 		ctx_req.rx_link_id = hapd->mld_link_id;
 	else
 #endif /* CONFIG_IEEE80211BE */
-		ctx_req.rx_link_id = -1;
+		ctx_req.rx_link_id = hapd->iface->current_hw_info->hw_idx;
 
 	/*
 	 * Fill assoc_req MLD topology if running as MLD AP
@@ -884,7 +890,7 @@ void hostapd_if_notify_disassoc(struct hostapd_data *hapd,
 		ctx_req.rx_link_id = hapd->mld_link_id;
 	else
 #endif /* CONFIG_IEEE80211BE */
-		ctx_req.rx_link_id = -1;
+		ctx_req.rx_link_id = hapd->iface->current_hw_info->hw_idx;
 
 	if ((policy != HOSTAPD_IF_FRAME_NOTIFY) &&
 		(policy != HOSTAPD_IF_FRAME_INVOKE))
@@ -929,7 +935,7 @@ void hostapd_if_notify_deauth(struct hostapd_data *hapd,
 		ctx_req.rx_link_id = hapd->mld_link_id;
 	else
 #endif /* CONFIG_IEEE80211BE */
-		ctx_req.rx_link_id = -1;
+		ctx_req.rx_link_id = hapd->iface->current_hw_info->hw_idx;
 
 	if ((policy != HOSTAPD_IF_FRAME_NOTIFY) &&
 	    (policy != HOSTAPD_IF_FRAME_INVOKE))
@@ -946,7 +952,7 @@ void hostapd_if_notify_deauth(struct hostapd_data *hapd,
 void hostapd_if_eapol_rx(struct hostapd_data *hapd, const u8 *sa,
 			 const u8 *data, u16 data_len)
 {
-	int link_id = -1;
+	int link_id = hapd->iface->current_hw_info->hw_idx;
 
 #ifdef CONFIG_IEEE80211BE
 	if (hapd->conf && hapd->conf->mld_ap)
@@ -1063,7 +1069,7 @@ hostapd_if_notify_action(struct hostapd_data *hapd,
 		link_id = hapd->mld_link_id;
 	else
 #endif /* CONFIG_IEEE80211BE */
-		link_id = -1;
+		link_id = hapd->iface->current_hw_info->hw_idx;
 
 	os_memset(&ctx_req, 0, sizeof(ctx_req));
 	ctx_req.rx_link_id = link_id;
@@ -1101,7 +1107,7 @@ hostapd_if_notify_action(struct hostapd_data *hapd,
 void hostapd_if_eapol_key_rx(struct hostapd_data *hapd, const u8 *sa,
 			     const u8 *data, u16 data_len)
 {
-	int link_id = -1;
+	int link_id = hapd->iface->current_hw_info->hw_idx;
 
 #ifdef CONFIG_IEEE80211BE
 	if (hapd->conf && hapd->conf->mld_ap)
@@ -2472,7 +2478,7 @@ void hostapd_if_event_deauth(struct hostapd_data *hapd, struct sta_info *sta,
 			     int tx_status_ok)
 {
 	struct hostapd_if_event evt;
-	int link_id = -1;
+	int link_id = hapd->iface->current_hw_info->hw_idx;
 
 	if (!hostapd_if_is_event_registered(hapd,
 					    HOSTAPD_IF_EVENT_DEAUTH))
@@ -2513,7 +2519,7 @@ void hostapd_if_event_disassoc(struct hostapd_data *hapd,
 			       int tx_status_ok)
 {
 	struct hostapd_if_event evt;
-	int link_id = -1;
+	int link_id = hapd->iface->current_hw_info->hw_idx;
 
 	if (!hostapd_if_is_event_registered(hapd,
 					    HOSTAPD_IF_EVENT_DISASSOC))
