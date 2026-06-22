greearb/hostap-ct

commit 2657e97c5234e3211bbb6cecc91c863d680e1efc

nl80211: Send link ID when starting CAC for radar detection
For MLO, link ID needs to be sent along with the nl80211 command to
start CAC for radar detection. Pass the link ID if operating as an AP
MLD.

Signed-off-by: Aditya Kumar Singh <quic_adisi@quicinc.com>

src/drivers/driver_nl80211.c
@@ -10599,9 +10599,23 @@ static int nl80211_start_radar_detection(void *priv,
 		return -1;
 	}
 
+	if (nl80211_link_valid(bss->valid_links, freq->link_id)) {
+		wpa_printf(MSG_DEBUG,
+			   "nl80211: Radar detection (CAC) on link_id=%d",
+			   freq->link_id);
+
+		if (nla_put_u8(msg, NL80211_ATTR_MLO_LINK_ID, freq->link_id)) {
+			nlmsg_free(msg);
+			return -1;
+		}
+	}
+
 	ret = send_and_recv_cmd(drv, msg);
-	if (ret == 0)
+	if (ret == 0) {
+		nl80211_link_set_freq(bss, freq->link_id, freq->freq);
 		return 0;
+	}
+
 	wpa_printf(MSG_DEBUG, "nl80211: Failed to start radar detection: "
 		   "%d (%s)", ret, strerror(-ret));
 	return -1;

src/ap/ap_drv_ops.c
@@ -1040,6 +1040,12 @@ int hostapd_start_dfs_cac(struct hostapd_iface *iface,
 	}
 	data.radar_background = radar_background;
 
+	data.link_id = -1;
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->conf->mld_ap)
+		data.link_id = hapd->mld_link_id;
+#endif /* CONFIG_IEEE80211BE */
+
 	res = hapd->driver->start_dfs_cac(hapd->drv_priv, &data);
 	if (!res) {
 		if (radar_background)

CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit 49fecba5f5a81443a2a7a99aa265e4602fa341f0

UPSTREAM:PROTOCOL:hostapd: Fix BSS freq corruption during background radar CAC
Skip nl80211_link_set_freq() in nl80211_start_radar_detection() when
radar_background is set. Background CAC runs on a secondary radio chain
without changing the operating channel, so updating bss->flink->freq with
the background CAC channel corrupts the cached BSS frequency.

Only update bss->flink->freq on a foreground radar CAC.

Tag: DFS
Fixes: 2657e97c5234 ("nl80211: Send link ID when starting CAC for radar detection")
Patch-dependency: None

Change-Id: I805bccfebd7bf3d8b86709746eba1975992db3d9
Signed-off-by: Thirusenthil Kumaran J <thirusen@qti.qualcomm.com>

src/drivers/driver_nl80211.c
@@ -12549,7 +12549,8 @@ static int nl80211_start_radar_detection(void *priv,
 
 	ret = send_and_recv_cmd(drv, msg);
 	if (ret == 0) {
-		nl80211_link_set_freq(bss, freq->link_id, freq->freq);
+		if (!freq->radar_background)
+			nl80211_link_set_freq(bss, freq->link_id, freq->freq);
 		return 0;
 	}
 