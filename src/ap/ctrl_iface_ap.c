516f060fde3033be3cf291debb6b8821d48ebd1e
Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>
2026-07-21 23:17:54 +0530

    UPSTREAM:PROTOCOL:hostapd: Fix build failure when CONFIG_TAXONOMY is disabled
    Currently, when CONFIG_TAXONOMY is disabled, is_wpa_oui(),
    is_wmm_oui(), and print_sta_ies_compact() are compiled
    unconditionally in ctrl_iface_ap.c. Their only call site is inside an
    existing #ifdef CONFIG_TAXONOMY block in hostapd_ctrl_iface_sta_mib().
    With the option disabled the call site is removed but the definitions
    remain, causing -Werror=unused-function.
    
    Fix this by wrapping the three function definitions with the same
    CONFIG_TAXONOMY guard as their call site.
    
    Patch-dependency: NA
    Patch-work: NA
    TAG: GENERAL
    Fixes: I948d4f2bce ("UPSTREAM:CP: hostapd: Added min_rssi and max_rssi to all_sta")
    Change-Id: Id32dd952b9b4c2b15ddec0dc7452adf5ab8aa593
    Signed-off-by: Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>
    (cherry picked from commit 59022fad61f795e85d69aabf12e622fc28b186e2)

--- src/ap/ctrl_iface_ap.c
+++ src/ap/ctrl_iface_ap.c
@@ -1452,6 +1452,7 @@ void check_and_add_uniibands(band_info_t uniiband)
 	}
 }
 
+#ifdef CONFIG_TAXONOMY
 static int is_wpa_oui(const u8 *ie)
 {
 	if (ie[1] < 4)
@@ -1524,6 +1525,7 @@ static int print_sta_ies_compact(const u8 *ies, size_t ies_len,
 
 	return len;
 }
+#endif /* CONFIG_TAXONOMY */
 static int hostapd_ctrl_iface_sta_mib(struct hostapd_data *hapd,
 				      struct sta_info *sta,
 				      char *buf, size_t buflen)
