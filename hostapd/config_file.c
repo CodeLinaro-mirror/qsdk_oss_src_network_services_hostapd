CodeLinaro-mirror/qsdk_oss_src_network_services_hostapd

commit ddf12748b52d64dce3c28fe3a8a7513eb7d5a684

UPSTREAM:CP:hostapd: add user override of dps assist
Dynamic Power Save (DPS) feature allows STA to operate in lower
capability mode during idle/listen period. AP will have the role
of DPS assist which means it will support transmission of an initial
Control frame for DPS operation.

Add hostapd config parameter for dps_assist that allows user to enable
or disable the feature. If driver has the feature disabled then user
override will have not effect.

If driver has DPS assist enabled but user wants to disable, include
dps assist parameter in start ap nl80211 command.

Patch-dependency: I85a95217b (UPSTREAM:PROTOCOL: hostapd: Add definitions for IEEE80211BN UHR)

Patch-work: None

Fixes: None

Change-Id: I2a274b5c82f7f4ab9a71c4ba67f65a0c2c13b099
Signed-off-by: Muna Sinada <muna.sinada@oss.qualcomm.com>

hostapd/config_file.c
@@ -5538,6 +5538,15 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 		conf->uhr_oper_chwidth = atoi(pos);
 	} else if (os_strcmp(buf, "uhr_oper_centr_freq_seg0_idx") == 0) {
 		conf->uhr_oper_centr_freq_seg0_idx = atoi(pos);
+	} else if (os_strcmp(buf, "dps_assist") == 0) {
+		int val;
+		val = atoi(pos);
+		if (val < FEATURE_DISABLED || val > FEATURE_ENABLED) {
+			wpa_printf(MSG_ERROR,
+				   "Invallid value for dps_assist");
+			return 1;
+		}
+		bss->dps_assist = val;
 #endif /* CONFIG_IEEE80211BN */
 
 	} else if (os_strcmp(buf, "enable_dscp_policy_capa") == 0) {

src/ap/ap_config.c
@@ -247,6 +247,10 @@ void hostapd_config_defaults_bss(struct hostapd_bss_config *bss)
 	/* Default: external plugin disabled (can be enabled per-BSS config) */
 	bss->external_plugin_enable = 0;
 #endif
+
+#ifdef CONFIG_IEEE80211BN
+	bss->dps_assist = DRIVER_DEFINED;
+#endif /* CONFIG_IEEE80211BN */
 }
 
 #ifdef CONFIG_IEEE80211BE

src/ap/ap_config.h
@@ -317,6 +317,10 @@ struct eht_phy_capabilities_info {
 };
 #endif /* CONFIG_IEEE80211BE */
 
+#define DRIVER_DEFINED		-1
+#define FEATURE_DISABLED	0
+#define FEATURE_ENABLED		1
+
 /**
  * struct hostapd_bss_config - Per-BSS configuration
  */
@@ -1195,6 +1199,15 @@ struct hostapd_bss_config {
 	 * Range: 1-100. Falls back to radio-wide setting if not set.
 	 */
 	int rssi_deauth_grace_samples;
+
+	/**
+	 * dps_assist - DPS Assist Support
+	 *
+	 * This is an override for per-BSS support for DPS Assist for AP.
+	 * If set to -1, fallback to driver support. If driver support is set,
+	 * 0 and 1 are used to enable and disable support.
+	 */
+	int dps_assist;
 };
 
 /**

src/ap/beacon.c
@@ -3927,7 +3927,13 @@ static int __ieee802_11_set_beacon(struct hostapd_data *hapd)
 		}
 	}
 #endif
-
+#ifdef CONFIG_IEEE80211BN
+	/* If driver support is enabled and user wants to disable feature then
+	 * we need to disable in driver
+	 */
+	if (hapd->conf->dps_assist == FEATURE_DISABLED)
+		params.dps_assist = FEATURE_DISABLED;
+#endif
 	if (hapd->iface->drv_flags2 & WPA_DRIVER_FLAGS2_CIGTK &&
 	    hapd->conf->control_frame_prot)
 		params.is_cfp_enabled = true;

src/drivers/driver_nl80211.c
@@ -6301,6 +6301,15 @@ static int wpa_driver_nl80211_set_ap(void *priv,
 			goto fail;
 	}
 
+#ifdef CONFIG_IEEE80211BN
+	if (!params->dps_assist) {
+		wpa_printf(MSG_DEBUG, "nl80211: disable DPS Assist");
+		if (nla_put_u8(msg, NL80211_ATTR_DPS_ASSIST,
+			       params->dps_assist))
+			goto fail;
+	}
+#endif /* CONFIG_IEEE80211BN */
+
 #ifdef CONFIG_DRIVER_NL80211_QCA
 	if (cmd == NL80211_CMD_NEW_BEACON && params->allowed_freqs)
 		qca_set_allowed_ap_freqs(bss, params->allowed_freqs,

hostapd/hostapd.conf
@@ -1439,6 +1439,11 @@ wmm_ac_vo_acm=0
 #disable_11bn: Boolean (0/1) to disable UHR for a specific BSS
 #disable_11bn=0
 
+#DPS Assist Support: Whether DPS Assist Support is enabled
+# 0 = disabled
+# 1 = enabled
+#dps_assist=1
+
 ##### IEEE 802.1X-2004 related configuration ##################################
 
 # Require IEEE 802.1X authorization

src/ap/ieee802_11_uhr.c
@@ -36,6 +36,12 @@ u8 * hostapd_eid_uhr_capab(struct hostapd_data *hapd, u8 *eid,
 	cap = (struct ieee80211_uhr_capabilities *)pos;
 	os_memset(cap, 0, sizeof(*cap));
 	os_memcpy(cap->mac_cap, uhr_cap->mac_cap, sizeof(cap->mac_cap));
+
+	/* Driver supports DPS Assist Support but disabled by user */
+	if ((uhr_cap->mac_cap[0] & UHR_MACCAP_DPS_ASSIST) &&
+	    hapd->conf->dps_assist == FEATURE_DISABLED)
+		cap->mac_cap[0] &= ~UHR_MACCAP_DPS_ASSIST;
+
 	os_memcpy(cap->phy_cap, uhr_cap->phy_cap, sizeof(cap->phy_cap));
 	pos += sizeof(struct ieee80211_uhr_capabilities);
 

src/common/ieee802_11_defs.h
@@ -3262,6 +3262,10 @@ struct ieee80211_uhr_operation {
 	u16 dps_oper_param; /*DPS Operation Parameters*/
 } STRUCT_PACKED;
 
+/* Figure 9-aa7: UHR MAC Capabilities Information field format */
+#define UHR_MACCAP_DPS_SUPP			BIT(0)
+#define UHR_MACCAP_DPS_ASSIST			BIT(1)
+
 #define UHR_MAC_CAPAB_LEN	5
 #define UHR_PHY_CAPAB_LEN	1
 /* Figure 9-aa4: UHR Capabilities element format P802.11bn_D1.0 section 9.4.2.aa2 */

commit 31b616942eb907558fa4ed812632ec151f4094b3

UPSTREAM:CP:hostapd: Resolve DPS Assist state from HW capability
Current behavior: dps_assist used a single field with
DRIVER_DEFINED=-1 as a sentinel value. The driver NL80211 path also
only sent dps_assist to the driver when the feature was disabled,
preventing the driver from learning the enabled state.

Remove the DRIVER_DEFINED sentinel and default dps_assist to
FEATURE_ENABLED. In hostapd.c, disable dps_assist if HW does not
support the feature; otherwise preserve the user-configured or default
enabled value. In beacon.c, send dps_assist to the driver whenever HW
supports the feature. In driver_nl80211.c, always send the DPS Assist
NL attribute regardless of enabled/disabled state.

Patch-dependency: ddf12748b5 (UPSTREAM:CP:hostapd: add user override of dps assist)

Fixes: ddf12748b5 (UPSTREAM:CP:hostapd: add user override of dps assist)

Change-Id: Ie27a338143c5e0a8a7c1e2c1f0606570771278f7
Signed-off-by: Muna Sinada <muna.sinada@oss.qualcomm.com>

hostapd/config_file.c
@@ -6200,7 +6200,7 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 		val = atoi(pos);
 		if (val < FEATURE_DISABLED || val > FEATURE_ENABLED) {
 			wpa_printf(MSG_ERROR,
-				   "Invallid value for dps_assist");
+				   "Invalid value for dps_assist");
 			return 1;
 		}
 		bss->dps_assist = val;

src/ap/ap_config.c
@@ -267,7 +267,7 @@ void hostapd_config_defaults_bss(struct hostapd_bss_config *bss)
 #endif
 
 #ifdef CONFIG_IEEE80211BN
-	bss->dps_assist = DRIVER_DEFINED;
+	bss->dps_assist = FEATURE_ENABLED;
 
 	/* UHR intervals are represented as TUs */
 	bss->uhr_params_update.adv_notification_interval = 10;

src/ap/ap_config.h
@@ -393,7 +393,6 @@ struct eht_phy_capabilities_info {
 };
 #endif /* CONFIG_IEEE80211BE */
 
-#define DRIVER_DEFINED		-1
 #define FEATURE_DISABLED	0
 #define FEATURE_ENABLED		1
 
@@ -1365,16 +1364,15 @@ struct hostapd_bss_config {
 	 */
 	int rssi_deauth_grace_samples;
 
+#ifdef CONFIG_IEEE80211BN
 	/**
 	 * dps_assist - DPS Assist Support
 	 *
-	 * This is an override for per-BSS support for DPS Assist for AP.
-	 * If set to -1, fallback to driver support. If driver support is set,
+	 * Per-BSS support for DPS Assist for AP. If driver support is set,
 	 * 0 and 1 are used to enable and disable support.
 	 */
 	int dps_assist;
 
-#ifdef CONFIG_IEEE80211BN
 	/**
 	 * uhr_params_update - UHR params update configuration
 	 *

src/ap/beacon.c
@@ -4632,11 +4632,10 @@ static int __ieee802_11_set_beacon(struct hostapd_data *hapd)
 #endif /* CONFIG_QCN_EXTN */
 #endif
 #ifdef CONFIG_IEEE80211BN
-	/* If driver support is enabled and user wants to disable feature then
-	 * we need to disable in driver
-	 */
-	if (hapd->conf->dps_assist == FEATURE_DISABLED)
-		params.dps_assist = FEATURE_DISABLED;
+	/* Send dps_assist to driver if HW supports the feature */
+	if (cmode && cmode->uhr_capab[IEEE80211_MODE_AP].uhr_supported &&
+	    (cmode->uhr_capab[IEEE80211_MODE_AP].mac_cap[0] & UHR_MACCAP_DPS_ASSIST))
+		params.dps_assist = hapd->conf->dps_assist;
 #endif
 	if (hapd->iface->drv_flags2 & WPA_DRIVER_FLAGS2_CIGTK &&
 	    hapd->conf->control_frame_prot)

src/ap/drv_callbacks.c
@@ -3900,6 +3900,25 @@ static void hostapd_update_ap_powersave(struct hostapd_data *hapd,
 					struct ap_powersave_event *ap_ps_event)
 {
 	if (ap_ps_event->dps_assist_updated) {
+		struct hostapd_hw_modes *mode = hapd->iface->current_mode;
+		struct uhr_capabilities *uhr_cap;
+
+		if (!mode) {
+			wpa_printf(MSG_ERROR,
+				   "Failed to %s DPS Assist feature due to feature not supported by HW",
+				   ap_ps_event->dps_assist ? "enable" : "disable");
+			return;
+		}
+
+		uhr_cap = &mode->uhr_capab[IEEE80211_MODE_AP];
+		if (!uhr_cap->uhr_supported ||
+		    !(uhr_cap->mac_cap[0] & UHR_MACCAP_DPS_ASSIST)) {
+			wpa_printf(MSG_ERROR,
+				   "Failed to %s DPS Assist feature due to feature not supported by HW",
+				   ap_ps_event->dps_assist ? "enable" : "disable");
+			return;
+		}
+
 		hapd->conf->dps_assist = ap_ps_event->dps_assist;
 		/* Update beacon with updated DPS Assist Support Bit */
 		if (ieee802_11_update_beacons(hapd->iface))

src/ap/hostapd.c
@@ -2747,10 +2747,28 @@ setup_mld:
 #endif /* CONFIG_QCN_EXTN */
 
 #ifdef CONFIG_IEEE80211BN
+	struct hostapd_hw_modes *mode;
+	struct uhr_capabilities *uhr_cap;
+
 	if (hostapd_is_uhr_enabled(hapd) &&
 	    ARRAY_SIZE(hapd->sta_aid) > 1) {
 		/* Reserve AIDs 56 to 63 in UHR for Critical Update */
 		hapd->sta_aid[1] |= 0xFF000000;
+	}
+
+	mode = hapd->iface->current_mode;
+	if (!mode) {
+		wpa_printf(MSG_ERROR,
+			   "Current hw mode mode not found, disabling DPS Assist");
+		hapd->conf->dps_assist = FEATURE_DISABLED;
+	} else {
+		uhr_cap = &mode->uhr_capab[IEEE80211_MODE_AP];
+
+		if (!uhr_cap->uhr_supported ||
+		    !(uhr_cap->mac_cap[0] & UHR_MACCAP_DPS_ASSIST)) {
+			wpa_printf(MSG_DEBUG, "Driver does not support DPS Assist");
+			hapd->conf->dps_assist = FEATURE_DISABLED;
+		}
 	}
 #endif /* CONFIG_IEEE80211BN */
 

src/drivers/driver_nl80211.c
@@ -6832,8 +6832,8 @@ wpa_driver_nl80211_set_ap_nlmsg_len(struct i802_bss *bss,
 		len += nl80211_attr_len_flag();
 
 #ifdef CONFIG_IEEE80211BN
-	if (!params->dps_assist)
-		len += nl80211_attr_len_u8();
+	/* DPS Assist parameter */
+	len += nl80211_attr_len_u8();
 #endif /* CONFIG_IEEE80211BN */
 
 	/* Len for NL80211_ATTR_SOCKET_OWNER */
@@ -7431,12 +7431,11 @@ static int wpa_driver_nl80211_set_ap(void *priv,
 		     params->uhr_cap + 3))
 		goto fail;
 
-	if (!params->dps_assist) {
-		wpa_printf(MSG_DEBUG, "nl80211: disable DPS Assist");
-		if (nla_put_u8(msg, NL80211_ATTR_DPS_ASSIST,
-			       params->dps_assist))
-			goto fail;
-	}
+	wpa_printf(MSG_DEBUG, "nl80211: %s DPS Assist",
+			   params->dps_assist? "Enable": "Disable");
+
+	if (nla_put_u8(msg, NL80211_ATTR_DPS_ASSIST, params->dps_assist))
+		goto fail;
 
        /* Set SMD parameters if configured */
        if (nl80211_put_smd_params(msg, &params->smd) < 0) {